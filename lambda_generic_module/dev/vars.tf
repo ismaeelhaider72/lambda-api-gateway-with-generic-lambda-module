59402e05e51f5b266d48d1e408d17ad2a27ddf2a
ismaeelhaider72 <66254080+ismaeelhaider72@users.noreply.github.com>
2021-08-27 16:27:59 +0500

    Update vars.tf

--- lambda_generic_module/dev/vars.tf
+++ lambda_generic_module/dev/vars.tf
@@ -1,20 +1,8 @@
-# variable "AWS_ACCESS_KEY" {
-#   default = "AKIAXEFPMSBJBTBVISO4"
 
-# }
-
-
-# variable "AWS_SECRET_KEY" {
-#   default = "OfJ+eFa0rlsy0awQ+6Ctr3ujK8sEL7qu2Ijy3Feh"
-# }
 
 variable "AWS_REGION" {
   default = "us-east-1"
 }
 
-
-variable "AWS_AccountId" {
-  default = "489994096722"
-}
 
 
